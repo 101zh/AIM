42ab9aea12829a7d3fe2faec4c06d6006336838b
101zh <67253838+101zh@users.noreply.github.com>
2024-12-30 14:44:06 -0800

    allow player to look in first person

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Vector3 moveDir = Vector3.zero;
     [SerializeField] float speed = 0.01f;
     CharacterController characterController;
+    Mouse curMouse;
+    Camera mainCam;
+    [SerializeField] float x;
+    [SerializeField] float y;
+    float cameraVerticalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        curMouse = Mouse.current;
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = curMouse.delta.x.value;
+        y = curMouse.delta.y.value;
+
+        cameraVerticalRotation -= y;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        mainCam.transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        transform.Rotate(Vector3.up * x);
         MovePlayer();
     }
 
